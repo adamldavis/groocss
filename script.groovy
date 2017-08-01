94044da7f7fcc9cb3746ac9151bf2dad5965a0b6
Adam Davis <ada.m.d.a.v@gmail.com>
2017-08-01 15:31:55 -0400

    Updated with 'Getting Started' section and added script.groovy

---
+++ script.groovy
@@ -0,0 +1,12 @@
+@Grab('org.groocss:groocss:0.10.1')
+import org.groocss.GrooCSS
+
+def css = GrooCSS.withConfig { prettyPrint() }.process {
+    a { textDecoration 'none' }
+    body _.content {
+        fontSize 20.px
+        width 400.px
+        display 'flex'
+    }
+}
+new File('main.css').text = "$css"
